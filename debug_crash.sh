c1ca2953663da6dbc51a6fe78b76b386873ff630
C.O.Marius <marrius9876@gmail.com>
2019-04-07 12:52:20 -0400

    Update debug_crash.sh

--- debug_crash.sh
+++ debug_crash.sh
@@ -1,20 +1,14 @@
-
-* CXX CC FLAGS   +=  '-DDEBUG -g'
-
-```
 #!/bin/bash
+
+# 
+# make sure the program has symbols: CXX & CC FLAGS   +=  '-DDEBUG -g'
 
 ulimit -S -c unlimited /path/to/binary/mybinary
 sysctl -w kernel.core_pattern=/home/path/to/binary/dump
-```
 
-* after crash go to /home/path/to/binary/dump
-
-```
-gdb -c dump-file-......
-
-(gdb)file /path/to/binary/mybinary
-(gdb) bt
-
-```
+######################################################################
+# after crash go to /home/path/to/binary/dump
+# gdb -c dump-file-......
+# (gdb)file /path/to/binary/mybinary
+# (gdb) bt
 
